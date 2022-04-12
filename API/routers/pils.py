42738ee6f27205e086f9daae353ccec86785420f
engelsman16 <teun.engels@gmail.com>
2022-04-12 20:26:55 +0200

    added new version of api

--- API/routers/pils.py
+++ API/routers/pils.py
@@ -17,7 +17,7 @@ def post_pils(post : schemas.PilsPost):
             pils = result.to_dict().get("pils")
             name = result.to_dict().get("naam")
             result = db.collection(u'drinkers').document(id).update({"pils": pils + 1})
-            return {f"{name} drink 1 more beer"}
+            return {f"{name} drank 1 more beer"}
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"{post.ChipID} not found")
 
